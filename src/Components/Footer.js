f634d89055127fe77e53579f0c52a49b49ce6ec1
oneseve <montoto14@gmail.com>
2022-02-17 10:53:05 +0100

    cambios

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2022 Mario Montoto</li>
+              <li>Diseñado por <a title="Styleshout" href="">Styleshout</a></li>
            </ul>
 
         </div>
